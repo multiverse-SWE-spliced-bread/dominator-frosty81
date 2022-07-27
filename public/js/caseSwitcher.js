bfcbaeade8edaee7cdadf02d45106d9543dfa89e
frosty81 <leonardminski@hotmail.co.uk>
2022-07-27 10:57:53 +0100

    Another attempt

--- public/js/caseSwitcher.js
+++ public/js/caseSwitcher.js
@@ -1,18 +1,21 @@
-  let para = document.getElementById("patrick");
-  let text = para.innerText;
+let para = document.getElementById("patrick");
+let text = para.innerText;
     
 const btn1 = document.getElementById("quiet-btn");
+
 function changeLower() {
   para.innerText = text.toLowerCase();
 }
 
 const btn2 = document.getElementById("loud-btn");
+
 function changeUpper() {
   para.innerText = para.innerText.toUpperCase();
 }
 
 
 const btn3 = document.getElementById("sarc-btn");
+
 function changeSarc() {
 let finalString = '';
 let counter = 0;
